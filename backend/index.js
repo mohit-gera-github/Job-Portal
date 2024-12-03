c4d35da7f55831e202b00d2b4f9fb70f4e7351df
Mohit Gera <mohitgera7714@gmail.com>
2024-12-03 17:40:53 +0530

    Made some changes

--- backend/index.js
+++ backend/index.js
@@ -16,11 +16,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
 
+const corsOptions = {
+    origin: process.env.CLIENT_URL || "http://localhost:5173", 
+    credentials: true,
+  };
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
@@ -34,7 +34,7 @@ app.use("/api/v1/application", applicationRoute);
 
 
 
-app.listen(PORT,()=>{
+app.listen(PORT,"0.0.0.0",()=>{
     connectDB();
     console.log(`Server running at port ${PORT}`);
 })
